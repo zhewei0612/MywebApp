3e57bc47986a3c7a9d2a13bf7956291dbe03c35e
zhewei0612 <154297788+zhewei0612@users.noreply.github.com>
2023-12-26 11:06:05 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,20 +4,27 @@ const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 const keys = document.querySelectorAll('.key')
 const whiteKeys = document.querySelectorAll('.key.white')
 const blackKeys = document.querySelectorAll('.key.black')
+const finger = document.querySelector(".finger");
 
 keys.forEach(key => {
-  key.addEventListener('click', () => playNote(key))
-})
+  key.addEventListener('click',
+  () => { playNote(key);
+          showFinger(key);})
 
-document.addEventListener('keydown', e => {
-  if (e.repeat) return
-  const key = e.key
-  const whiteKeyIndex = WHITE_KEYS.indexOf(key)
-  const blackKeyIndex = BLACK_KEYS.indexOf(key)
+  key.addEventListener("mouseup", function () {
+            hideFinger(); // 隱藏手指頭
+          });
+        })
 
-  if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
-  if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
-})
+// document.addEventListener('keydown', e => {
+//   if (e.repeat) return
+//   const key = e.key
+//   const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+//   const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+//   if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+//   if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+// })
 
 function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note)
@@ -27,4 +34,15 @@ function playNote(key) {
   noteAudio.addEventListener('ended', () => {
     key.classList.remove('active')
   })
+}
+
+function showFinger(key) {
+  const keyRect = key.getBoundingClientRect();
+  finger.style.left = keyRect.left + keyRect.width / 2 - 25 + "px";
+  finger.style.top = keyRect.bottom + "px";
+  finger.style.display = "block";
+}
+
+function hideFinger() {
+  finger.style.display = "none";
 }
